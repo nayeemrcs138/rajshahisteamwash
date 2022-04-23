a9c48b75ad7bf303ebef523eaaba4d3dafdabc7b
nayeemrcs138 <47120398+nayeemrcs138@users.noreply.github.com>
2022-04-24 04:02:26 +0600

    Add files via upload

---
+++ admin/includes/header.php
@@ -0,0 +1,31 @@
+<div class="header-main">
+				<div class="logo-w3-agile" style="height: 120px; background: black;">
+					<a href="dashboard.php"> <img src="images/logo.png" style="width: 200px; height: 80px;"> </a>
+				</div>
+				
+						
+						<div class="profile_details w3l" style="height: 120px; background: #f98f01;">		
+								<ul>
+									<li class="dropdown profile_details_drop">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+											<div class="profile_img">	
+												<span class="prfil-img"><img src="images/User-icon.png" alt=""> </span> 
+												<div class="user-name">
+													<p>Welcome</p>
+													<span>Admin</span>
+												</div>
+												<i class="fa fa-angle-down"></i>
+												<i class="fa fa-angle-up"></i>
+												<div class="clearfix"></div>	
+											</div>	
+										</a>
+										<ul class="dropdown-menu drp-mnu">
+											<li> <a href="change-password.php"><i class="fa fa-lock"></i> Setting</a> </li> 
+											<li> <a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a> </li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+							
+				 <div class="clearfix"> </div>	
+</div>
---
+++ admin/includes/sidebarmenu.php
@@ -0,0 +1,60 @@
+<div class="sidebar-menu">
+	<header class="logo1" style="background: #f98f01;">
+		<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a>
+	</header>
+	<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+	<div class="menu">
+		<ul id="menu">
+			<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span>
+					<div class="clearfix"></div>
+				</a></li>
+
+			<li id="menu-academico"><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span>Washing Points</span> <span class="fa fa-angle-right" style="float: right"></span>
+					<div class="clearfix"></div>
+				</a>
+				<ul id="menu-academico-sub">
+					<li id="menu-academico-avaliacoes"><a href="addcar-washpoint.php">Add</a></li>
+					<li id="menu-academico-avaliacoes"><a href="managecar-washingpoints.php">Manage</a></li>
+				</ul>
+			</li>
+
+			<li><a href="add-booking.php"><i class="fa fa-user" aria-hidden="true"></i> <span>Add Car Wash Booking</span>
+					<div class="clearfix"></div>
+				</a></li>
+
+
+			<li id="menu-academico"><a href="#"><i class="fa fa-files-o" aria-hidden="true"></i><span>Car Wash Booking</span> <span class="fa fa-angle-right" style="float: right"></span>
+					<div class="clearfix"></div>
+				</a>
+				<ul id="menu-academico-sub">
+					<li id="menu-academico-avaliacoes"><a href="pending-bookings.php">Pending</a></li>
+					<li id="menu-academico-avaliacoes"><a href="new-booking.php">New</a></li>
+					<li id="menu-academico-avaliacoes"><a href="completed-booking.php">Completed</a></li>
+				</ul>
+			</li>
+
+
+			<li id="menu-academico"><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span>Package</span> <span class="fa fa-angle-right" style="float: right"></span>
+					<div class="clearfix"></div>
+				</a>
+				<ul id="menu-academico-sub">
+					<li id="menu-academico-avaliacoes"><a href="add-services.php">Add</a></li>
+					<li id="menu-academico-avaliacoes"><a href="manage-services.php">Manage</a></li>
+				</ul>
+			</li>
+
+
+			<li><a href="manage-enquires.php"><i class="fa fa-file-text-o" aria-hidden="true"></i> <span>Manage Enquiries</span>
+					<div class="clearfix"></div>
+				</a></li>
+			<li id="menu-academico"><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span>Pages</span> <span class="fa fa-angle-right" style="float: right"></span>
+					<div class="clearfix"></div>
+				</a>
+				<ul id="menu-academico-sub">
+					<li id="menu-academico-avaliacoes"><a href="news.php">News & Events</a></li>
+			</li>
+
+
+		</ul>
+	</div>
+</div>
